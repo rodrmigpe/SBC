f8d37d14211425b698b34ce66875ccffcd7dd757
rodrmigpe <57412494+rodrmigpe@users.noreply.github.com>
2021-05-21 01:01:04 +0100

    Fixing some issues

---
+++ TarefaA_Parte1/base_conhecimento.pl
@@ -0,0 +1,239 @@
+:- dynamic(fact/1).
+:- [backward,forward,bd,proof].
+
+if 'nao' and 'normal' and 'fastFood' and '1-10' and 'diasEspeciais' then 'perfil1'.
+
+if 'nao' and 'normal' and 'fastFood' and '1-10' and 'dieta' then 'perfil2'.
+
+if 'nao' and 'normal' and 'fastFood' and '1-10' and 'algumasVezes' then 'perfil3'.
+
+if 'nao' and 'normal' and 'fastFood' and '11-20' and 'diasEspeciais' then 'perfil4'.
+
+if 'nao' and 'normal' and 'fastFood' and '11-20' and 'dieta' then 'perfil5'.
+
+if 'nao' and 'normal' and 'fastFood' and '11-20' and 'algumasVezes' then 'perfil7'.
+
+if 'nao' and 'normal' and 'fastFood' and '20+' and 'diasEspeciais' then 'perfil8'.
+
+if 'nao' and 'normal' and 'fastFood' and '20+' and 'dieta' then 'perfil9'.
+
+if 'nao' and 'normal' and 'fastFood' and '20+' and 'algumasVezes' then 'perfil10'.
+
+if 'nao' and 'normal' and 'vegetariana' and '1-10' and 'diasEspeciais' then 'perfil11'.
+
+if 'nao' and 'normal' and 'vegetariana' and '1-10' and 'dieta' then 'perfil12'.
+
+if 'nao' and 'normal' and 'vegetariana' and '1-10' and 'algumasVezes' then 'perfil13'.
+
+if 'nao' and 'normal' and 'vegetariana' and '11-20' and 'diasEspeciais' then 'perfil14'.
+
+if 'nao' and 'normal' and 'vegetariana' and '11-20' and 'dieta' then 'perfil15'.
+
+if 'nao' and 'normal' and 'vegetariana' and '11-20' and 'algumasVezes' then 'perfil16'.
+
+if 'nao' and 'normal' and 'vegetariana' and '20+' and 'diasEspeciais' then 'perfil17'.
+
+if 'nao' and 'normal' and 'vegetariana' and '20+' and 'dieta' then 'perfil18'.
+
+if 'nao' and 'normal' and 'vegetariana' and '20+' and 'algumasVezes' then 'perfil19'.
+
+if 'nao' and 'normal' and 'comidaTradicional' and '1-10' and 'diasEspeciais' then 'perfil20'.
+
+if 'nao' and 'normal' and 'comidaTradicional' and '1-10' and 'dieta' then 'perfil21'.
+
+if 'nao' and 'normal' and 'comidaTradicional' and '1-10' and 'algumasVezes' then 'perfil22'.
+
+if 'nao' and 'normal' and 'comidaTradicional' and '11-20' and 'diasEspeciais' then 'perfil22'.
+
+if 'nao' and 'normal' and 'comidaTradicional' and '11-20' and 'dieta' then 'perfil23'.
+
+if 'nao' and 'normal' and 'comidaTradicional' and '11-20' and 'algumasVezes' then 'perfil24'.
+
+if 'nao' and 'normal' and 'comidaTradicional' and '20+' and 'diasEspeciais' then 'perfil25'.
+
+if 'nao' and 'normal' and 'comidaTradicional' and '20+' and 'dieta' then 'perfil26'.
+
+if 'nao' and 'normal' and 'comidaTradicional' and '20+' and 'algumasVezes' then 'perfil27'.
+
+if 'nao' and 'normal' and 'comidaOriental' and '1-10' and 'diasEspeciais' then 'perfil28'.
+
+if 'nao' and 'normal' and 'comidaOriental' and '1-10' and 'dieta' then 'perfil29'.
+
+if 'nao' and 'normal' and 'comidaOriental' and '1-10' and 'algumasVezes' then 'perfil30'.
+
+if 'nao' and 'normal' and 'comidaOriental' and '11-20' and 'diasEspeciais' then 'perfil31'.
+
+if 'nao' and 'normal' and 'comidaOriental' and '11-20' and 'dieta' then 'perfil32'.
+
+if 'nao' and 'normal' and 'comidaOriental' and '11-20' and 'algumasVezes' then 'perfil33'.
+
+if 'nao' and 'normal' and 'comidaOriental' and '20+' and 'diasEspeciais' then 'perfil34'.
+
+if 'nao' and 'normal' and 'comidaOriental' and '20+' and 'dieta' then 'perfil35'.
+
+if 'nao' and 'normal' and 'comidaOriental' and '20+' and 'algumasVezes' then 'perfil36'.
+
+if 'nao' and 'vegan' and 'fastFood' and '1-10' and 'diasEspeciais' then 'perfil37'.
+
+if 'nao' and 'vegan' and 'fastFood' and '1-10' and 'dieta' then 'perfil38'. 
+
+if 'nao' and 'vegan' and 'fastFood' and '1-10' and 'algumasVezes' then 'perfil39'. 
+
+if 'nao' and 'vegan' and 'fastFood' and '11-20' and 'diasEspeciais' then 'perfil40'. 
+
+if 'nao' and 'vegan' and 'fastFood' and '11-20' and 'dieta' then 'perfil41'. 
+
+if 'nao' and 'vegan' and 'fastFood' and '11-20' and 'algumasVezes' then 'perfil42'.
+
+if 'nao' and 'vegan' and 'fastFood' and '20+' and 'diasEspeciais' then 'perfil43'.
+
+if 'nao' and 'vegan' and 'fastFood' and '20+' and 'dieta' then 'perfil44'. 
+
+if 'nao' and 'vegan' and 'fastFood' and '20+' and 'algumasVezes ' then 'perfil45'. 
+
+if 'nao' and 'vegan' and 'vegetariana' and '1-10' and 'diasEspeciais' then 'perfil46'. 
+
+if 'nao' and 'vegan' and 'vegetariana' and '1-10' and 'dieta' then 'perfil47'. 
+
+if 'nao' and 'vegan' and 'vegetariana' and '1-10' and 'algumasVezes' then 'perfil48'.
+
+if 'nao' and 'vegan' and 'vegetariana' and '11-20' and 'diasEspeciais' then 'perfil49'. 
+
+if 'nao' and 'vegan' and 'vegetariana' and '11-20' and 'dieta' then 'perfil50'. 
+
+if 'nao' and 'vegan' and 'vegetariana' and '11-20' and 'algumasVezes' then 'perfil51'. 
+
+if 'nao' and 'vegan' and 'vegetariana' and '20+' and 'dieta' then 'perfil52'.
+
+if 'nao' and 'vegan' and 'vegetariana' and '20+' and 'diasEspeciais' then 'perfil53'.
+
+if 'nao' and 'vegan' and 'vegetariana' and '20+' and 'algumasVezes' then'perfil54'.
+
+if 'nao' and 'vegan' and 'comidaTradicional' and '1-10' and 'dieta' then 'perfil55'.
+
+if 'nao' and 'vegan' and 'comidaTradicional' and '1-10' and 'diasEspeciais' then 'perfil56'. 
+
+if 'nao' and 'vegan' and 'comidaTradicional' and '1-10' and 'algumasVezes' then 'perfil57'.
+
+if 'nao' and 'vegan' and 'comidaTradicional' and '11-20' and 'algumasVezes' then 'perfil58'.
+
+if 'nao' and 'vegan' and 'comidaTradicional' and '11-20' and 'dieta' then 'perfil59'.
+
+if 'nao' and 'vegan' and 'comidaTradicional' and '11-20' and 'diasEspeciais' then 'perfil60'.
+
+if 'nao' and 'vegan' and 'comidaTradicional' and '20+' and 'algumasVezes' then 'perfil61'.
+
+if 'nao' and 'vegan' and 'comidaTradicional' and '20+' and 'dieta' then 'perfil62'.
+
+if 'nao' and 'vegan' and 'comidaTradicional' and '20+' and 'diasEspeciais' then 'perfil63'.
+
+if 'nao' and 'vegan' and 'comidaOriental' and '1-10' and 'dieta' then 'perfil64'.
+
+if 'nao' and 'vegan' and 'comidaOriental' and '1-10' and 'diasEspeciais' then 'perfil65'.
+
+if 'nao' and 'vegan' and 'comidaOriental' and '1-10' and 'algumasVezes' then 'perfil66'.
+
+if 'nao' and 'vegan' and 'comidaOriental' and '11-20' and 'dieta' then 'perfil67'.
+
+if 'nao' and 'vegan' and 'comidaOriental' and '11-20' and 'diasEspeciais' then 'perfil68'.
+
+if 'nao' and 'vegan' and 'comidaOriental' and '11-20' and 'algumasVezes' then 'perfil69'.
+
+if 'nao' and 'vegan' and 'comidaOriental' and '20+' and 'dieta' then 'perfil70'.
+
+if 'nao' and 'vegan' and 'comidaOriental' and '20+' and 'diasEspeciais' then 'perfil71'.
+
+if 'nao' and 'vegan' and 'comidaOriental' and '20+' and 'algumasVezes' then 'perfil72'.
+
+if 'nao' and 'vegetariano' and 'fastFood' and '1-10' and 'diasEspeciais' then 'perfil73'.
+
+if 'nao' and 'vegetariano' and 'vegetariana' and '1-10' and 'diasEspeciais' then 'perfil74'.
+
+if 'nao' and 'vegetariano' and 'comidaTradicional' and '1-10' and 'diasEspeciais' then 'perfil75'.
+
+if 'nao' and 'vegetariano' and 'comidaOriental' and '1-10' and 'diasEspeciais' then 'perfil76'.
+
+if 'nao' and 'vegetariano' and 'fastFood' and '11-20' and 'diasEspeciais' then 'perfil77'.
+
+if 'nao' and 'vegetariano' and 'fastFood' and '20+' and 'diasEspeciais' then 'perfil78'.
+
+if 'nao' and 'vegetariano' and 'vegetariana' and '11-20' and 'diasEspeciais' then 'perfil79'.
+
+if 'nao' and 'vegetariano' and 'vegetariana' and '20+' and 'diasEspeciais' then 'perfil80'.
+
+if 'nao' and 'vegetariano' and 'comidaTradicional' and '11-20' and 'diasEspeciais' then 'perfil81'.
+
+if 'nao' and 'vegetariano' and 'comidaTradicional' and '20+' and 'diasEspeciais' then 'perfil82'.
+
+if 'nao' and 'vegetariano' and 'comidaOriental' and '11-20' and 'diasEspeciais' then 'perfil83'.
+
+if 'nao' and 'vegetariano' and 'comidaOriental' and '20+' and 'diasEspeciais' then 'perfil84'.
+
+if 'nao' and 'vegetariano' and 'fastFood' and '1-10' and 'dieta' then 'perfil85'.
+
+if 'nao' and 'vegetariano' and 'fastFood' and '1-10' and 'algumasVezes' then 'perfil86'.
+
+if 'nao' and 'vegetariano' and 'vegetariana' and '1-10' and 'dieta' then 'perfil87'.
+
+if 'nao' and 'vegetariano' and 'vegetariana' and '1-10' and 'algumasVezes' then 'perfil88'.
+
+if 'nao' and 'vegetariano' and 'comidaTradicional' and '1-10' and 'dieta' then 'perfil89'.
+
+if 'nao' and 'vegetariano' and 'comidaTradicional' and '1-10' and 'algumasVezes' then 'perfil90'.
+
+if 'nao' and 'vegetariano' and 'comidaOriental' and '1-10' and 'dieta' then 'perfil91'.
+
+if 'nao' and 'vegetariano' and 'comidaOriental' and '1-10' and 'algumasVezes' then 'perfil92'.
+
+if 'nao' and 'vegetariano' and 'fastFood' and '11-20' and 'dieta' then 'perfil93'.
+
+if 'nao' and 'vegetariano' and 'fastFood' and '11-20' and 'algumasVezes' then 'perfil94'.
+
+if 'nao' and 'vegetariano' and 'vegetariana' and '11-20' and 'dieta' then 'perfil95'.
+
+if 'nao' and 'vegetariano' and 'vegetariana' and '11-20' and 'algumasVezes' then 'perfil96'.
+
+if 'nao' and 'vegetariano' and 'comidaTradicional' and '11-20' and 'dieta' then 'perfil97'.
+
+if 'nao' and 'vegetariano' and 'comidaTradicional' and '11-20' and 'algumasVezes' then 'perfil98'.
+
+if 'nao' and 'vegetariano' and 'comidaOriental' and '1-10' and 'dieta' then 'perfil99'.
+
+if 'nao' and 'vegetariano' and 'comidaOriental' and '11-20' and 'algumasVezes' then 'perfil100'.
+
+if 'nao' and 'vegetariano' and 'fastFood' and '20+' and 'dieta' then 'perfil101'.
+
+if 'nao' and 'vegetariano' and 'fastFood' and '20+' and 'algumasVezes' then 'perfil102'.
+
+if 'nao' and 'vegetariano' and 'vegetariana' and '20+' and 'dieta' then 'perfil103'.
+
+if 'nao' and 'vegetariano' and 'vegetariana' and '20+' and 'algumasVezes' then 'perfil104'.
+
+if 'nao' and 'vegetariano' and 'comidaTradicional' and '20+' and 'dieta' then 'perfil105'.
+
+if 'nao' and 'vegetariano' and 'comidaTradicional' and '20+' and 'algumasVezes' then 'perfil106'.
+
+if 'nao' and 'vegetariano' and 'comidaOriental' and '20+' and 'dieta' then 'perfil107'.
+
+if 'nao' and 'vegetariano' and 'comidaOriental' and '20+' and 'algumasVezes' then 'perfil108'.
+
+if 'sim' and 'noGluten' and 'fastFood' then 'perfil109'.
+
+if 'sim' and 'noGluten' and 'vegetariana' then 'perfil110'.
+
+if 'sim' and 'noGluten' and 'comidaTradicional' then 'perfil111'.
+
+if 'sim' and 'noGluten' and 'comidaOriental' then 'perfil112'.
+
+if 'sim' and 'noLactose' and 'fastFood' then 'perfil113'.
+
+if 'sim' and 'noLactose' and 'vegetariana' then 'perfil114'.
+
+if 'sim' and 'noLactose' and 'comidaTradicional' then 'perfil115'.
+
+if 'sim' and 'noLactose' and 'comidaOriental' then 'perfil116'.
+
+opcoes(PERFIL):- fact(PRATO), refeicao(PRATO,PERFIL).
+opcoes(_).
+
+resposta(P):- demo, findall(PERFIL,opcoes(PERFIL),L1), list_to_set(L1,P).
